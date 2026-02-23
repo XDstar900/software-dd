b96b7ea96be539e12f33f2d0c59198835bf52a2f
XDstar900 <ayan.shahid421@education.nsw.gov.au>
2026-02-24 09:41:29 +1100

    aewsrdt

---
+++ tester.py
@@ -0,0 +1,6 @@
+import tkinter as tk
+root = tk.Tk()
+root.geometry("800x600")
+root.title("TESTER")
+label = tk.Label(root, text= "kill all", font = ("Arial", 18))
+root.mainloop() # activates window
