af8f32049e2598c0043defd91ba8d02e1259869b
XDstar900 <ayan.shahid421@education.nsw.gov.au>
2026-02-26 11:49:53 +1100

    urhuierhfuer

--- Untitled-1.py
+++ Untitled-1.py
@@ -8,10 +8,9 @@ Log_in_label = Label(UI, text="Log in", font=("Arial", 24, "bold"), bg="#00BAFF"
 Log_in_label.pack(pady=20)
 
 Username_label = Label(UI, text="Username", font=("Arial", 18),  bg="#00BAFF")
-Username_label.pack(pady = 10, side= LEFT,)
+Username_label.pack(pady = 10, side= TOP,anchor="w")
 
 Password_label = Label(UI, text="Password", font=("Arial", 18), bg="#00BAFF")
-Password_label.pack(pady= 10, side = LEFT,)
-Password_label.lower(Username_label)  # Makes widget1 go under widget2   
+Password_label.pack(pady= 10, side = BOTTOM,anchor= "w")
 
 UI.mainloop() #loads the window
