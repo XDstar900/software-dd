e3a0ee2d7f92da767aa8e76550d2d21c95f0b57b
XDstar900 <ayan.shahid421@education.nsw.gov.au>
2026-02-26 11:41:08 +1100

    labels set up. Please add entry box and start documenting some erros

--- Untitled-1.py
+++ Untitled-1.py
@@ -1,15 +1,17 @@
-import tkinter as tk
-UI = tk.Tk()
+from tkinter import * 
+UI = Tk()
+User = Entry()
+
 UI.title("KFC training program")
-Icon = tk.PhotoImage(file="kfc-icon.png")
-UI.iconphoto(True, Icon)
-UI.geometry("800x600")
 UI.configure(bg = "#00BAFF")
-Log_in_label = tk.Label(UI, text="Log in", font=("Arial", 24, "bold"), bg="#00BAFF")
+Log_in_label = Label(UI, text="Log in", font=("Arial", 24, "bold"), bg="#00BAFF")
 Log_in_label.pack(pady=20)
-Username_label = tk.Label(UI, text="Username", font=("Arial", 18),  bg="#00BAFF")
-Username_label.pack(pady = 10)
-Password_label = tk.Label(UI, text="Password", font=("Arial", 18), bg="#00BAFF")
-Password_label.pack(pady= 10)
+
+Username_label = Label(UI, text="Username", font=("Arial", 18),  bg="#00BAFF")
+Username_label.pack(pady = 10, side= LEFT,)
+
+Password_label = Label(UI, text="Password", font=("Arial", 18), bg="#00BAFF")
+Password_label.pack(pady= 10, side = LEFT,)
+Password_label.lower(Username_label)  # Makes widget1 go under widget2   
 
 UI.mainloop() #loads the window
