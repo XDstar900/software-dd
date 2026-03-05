b35bd8b14cd26d6e8cb6e49d78af6914e6690b89
XDstar900 <ayan.shahid421@education.nsw.gov.au>
2026-03-05 11:56:26 +1100

    Set up function for getting quizzed and make sure it ends if they went to shop to prevent crashes.

--- Untitled-1.py
+++ Untitled-1.py
@@ -45,14 +45,74 @@ Log_in_label.grid(row=3, column=0, columnspan=2, padx=10, pady=5)
 UI.mainloop() #loads the window
 
 while True:
-        Course = input("Choose an option using the letters in the brackets:\n (A) Chicken handling \n (B) Health and safety procedures \n (C) Go to shop \n").upper()
-        match Course: # casewhere statement in python to check the user's input
-            case "A":
-                print("Chicken handling")
-            case "B":
-                print("Health and safety procedures")
-            case "C":
-                print("Go to shop")
-            case _:
-                print("Invalid option. Please choose A, B, or C.")
-                continue # loops back to the input prompt if an invalid option is chosen
+    Course = input("Choose an option using the letters in the brackets:\n (A) Chicken cooking and handling \n (B) Health and safety procedures \n (C) Go to shop \n").upper()
+    match Course: # casewhere statement in python to check the user's input
+        case "A":
+            print("Chicken cooking and handling")
+            #QA_Bank is a list of dictionaries, each conaitng all the information required for question 
+            QA_Bank = ({
+                    'id': 1,
+                    'scenario': 'The refrigerator temperature reads 8°C. Should you:',
+                    'options': {
+                        'A': 'Continue using it - close enough',
+                        'B': 'Report it to management immediately',
+                        'C': 'Try to adjust temperature yourself',
+                        'D': 'Move all food to another fridge'
+                    },
+                    'correct_answer': 'B',
+                    'explanation': 'Temperature control issues must be reported immediately.',
+                    'is_challenge': False
+                },
+            {
+                "id": 2,
+            "scenario": "You have have the following batches to cook: 1 bag of OR chicken, 2 bags of tenders, and 1 bag of zinger fillets.\n Which batch should you cook first?",
+            "options": {
+                    "A": "Cook the OR chicken first",
+                    "B": "Cook the tenders first",
+                    "C": "Cook the zinger fillets first",
+                    "D": "Cook whichever batch you feel like first"
+            },
+            "correct_answer": "A",
+            "explanation": "The OR chicken has takes the longest time to cook so it should be cooked first, unless your manger specifies otherwise.",
+            "is_challenge": True
+            },
+            {
+            'id': 3,
+            'scenario': 'What colour chux do you need to use to clean the fryers after dropping a batch of chicken?',
+            'options': {
+                'A': "Green",
+                'B': "Blue",
+                'C': 'Red',
+                'D': "Purple"
+            },
+            'correct_answer': 'C',
+            'explanation': "You must always use the Red chux after to clean the fryers to prevent decomtamination.",
+            'is_challenge': False
+            },
+            {
+            'id': 4,
+            'scenario':  "It is a busy night and we need to make sure the chicken texture is as good as possible. What strategy do we use to maintain the flour to prevent clumps? ",
+            'options': {
+                'A': "Sift and top the lugs up as you alternate between them, when they need to.",
+                'B': "Save sifting for the end" ,
+                'C': "Only do it when you need to",
+                'D': "Do it after every bag", 
+            },
+            'correct_answer': "A",
+            'explanation': "Sifting and topping up the flour as you go, ensures that the flour is top quality while allowing you to bread all the chicken quickly.",
+            'is_challenge': True
+            },
+
+
+            )
+        case "B":
+            print("Health and safety procedures")
+        case "C":
+            print("Go to shop")
+            QA_Bank = False # resets the question bank to prevent user from accessing it when they go to the shop and prevents crashing when they come out of shop. 
+        case _:
+            print("Invalid option. Please choose A, B, or C.")
+            continue # loops back to the input prompt if an invalid option is chosen
+        
+    
+    
--- tester.py
+++ tester.py
@@ -1,6 +1,7 @@
-safety_questions = [{
+import random
+QA_Bank = [{
          'id': 1,
-         'scenario': 'The refrigerator temperature reads 8°C (safe is below 5°C)',
+         'scenario': 'The refrigerator temperature reads 8°C. Should you:',
          'options': {
                'A': 'Continue using it - close enough',
                'B': 'Report it to management immediately',
@@ -10,20 +11,48 @@ safety_questions = [{
          'correct_answer': 'B',
          'explanation': 'Temperature control issues must be reported immediately.',
          'is_challenge': False
-      }
-      {
-          "id": 2,
-         "scenario": "You have have the following batches to cook: 1 bag of OR chicken, 2 bags of tenders, and 1 bag of zinger fillets.\n Which batch should you cook first?",
-         "options": {
-               "A": "Cook the OR chicken first",
-               "B": "Cook the tenders first",
-               "C": "Cook the zinger fillets first",
-               "D": "Cook whichever batch you feel like first"
+      },
+{
+    "id": 2,
+   "scenario": "You have have the following batches to cook: 1 bag of OR chicken, 2 bags of tenders, and 1 bag of zinger fillets.\n Which batch should you cook first?",
+   "options": {
+         "A": "Cook the OR chicken first",
+         "B": "Cook the tenders first",
+         "C": "Cook the zinger fillets first",
+         "D": "Cook whichever batch you feel like first"
    },
-         "correct_answer": "A",
-         "explanation": "The OR chicken has takes the longest time to cook so it should be cooked first, unless your manger specifies otherwise.",
-         "is_challenge": True
-      }]
-      
+   "correct_answer": "A",
+   "explanation": "The OR chicken has takes the longest time to cook so it should be cooked first, unless your manger specifies otherwise.",
+   "is_challenge": True
+},
+{
+   'id': 3,
+   'scenario': 'What colour chux do you need to use to clean the fryers after dropping a batch of chicken?',
+   'options': {
+      'A': "Green",
+      'B': "Blue",
+      'C': 'Red',
+      'D': "Purple"
+   },
+   'correct_answer': 'C',
+   'explanation': "You must always use the Red chux after to clean the fryers to prevent decomtamination.",
+   'is_challenge': False
+},
+{
+   'id': 4,
+   'scenario':  "It is a busy night and we need to make sure the chicken texture is as good as possible. What strategy do we use to maintain the flour to prevent clumps? ",
+   'options': {
+      'A': "Sift and top the lugs up as you alternate between them, when they need to.",
+      'B': "Save sifting for the end" ,
+      'C': "Only do it when you need to",
+      'D': "Do it after every bag", 
+   },
+   'correct_answer': "A",
+   'explanation': "Sifting and topping up the flour as you go, ensures that the flour is top quality while allowing you to bread all the chicken quickly.",
+   'is_challenge': True
+},
+
+
+]
 
 
