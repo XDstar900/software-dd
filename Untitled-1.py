502a26d59fe915708430019cce469cf6ad82d016
XDstar900 <ayan.shahid421@education.nsw.gov.au>
2026-03-10 10:44:49 +1100

    ruigheruigberigher

--- Untitled-1.py
+++ Untitled-1.py
@@ -7,8 +7,9 @@ items = { # dictionary of items in the shop and their prices. Structure is item:
     "🛡️":(30,3),
 }
 def shop():
-    print(f"Welcome to the shop!\nYou have {wallet} points to spend.\nPlease type the number in the brackets next to the item you want to purchase:")
+    print(f"Welcome to the shop!\nYou have {wallet} points to spend.\n Please choose one of the following options:\nBuy\t(A)\nView owned items\n Leave")
     for item, (price, input_number) in items.items(): #Loops though items in the shop and displays them in a readable format, with the price and the item.
         print(f"{item}: {price} points ({input_number})")
+        Purchase = input("Please inpu ")
     
 shop() 
